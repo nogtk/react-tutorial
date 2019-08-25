dd7527c867103589a1fdc6f305ae866d61b4701a
nogtk <taka.naoga@moneyforward.co.jp>
2019-08-25 21:16:29 +0900

    Add footer component

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default class Layout extends React.Component {
     render() {
+        let components = [<Header />, <Footer />];
         return (
             <div>
-                <Header />
+                { components }
             </div>
         );
     }
