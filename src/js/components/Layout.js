5b08105a802c409f60764ed73209478500b99657
nogtk <taka.naoga@moneyforward.co.jp>
2019-08-25 21:11:06 +0900

    Add header component

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 export default class Layout extends React.Component {
-    constructor() {
-        super();
-        this.name = "Taka";
-    }
     render() {
         return (
-            <h1>Hello! { this.name }</h1>
+            <div>
+                <Header />
+            </div>
         );
     }
 }
