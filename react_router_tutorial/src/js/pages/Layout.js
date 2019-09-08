1d8c8c8ce800e35678ad93261c895d9fd9000871
nogtk <taka.naoga@moneyforward.co.jp>
2019-09-08 22:29:05 +0900

    Get query string

--- react_router_tutorial/src/js/pages/Layout.js
+++ react_router_tutorial/src/js/pages/Layout.js
@@ -11,8 +11,8 @@ class Layout extends React.Component {
       <div>
         <h1>KillerNew.net</h1>
         {this.props.children}
-        <Link to="/archives/some-other-articles" class="btn btn-warning">archives (some other articles)</Link>
-        <Link to="archives"><button class="btn btn-danger">archives</button></Link>,
+        <Link to="/archives/some-other-articles?date=yesterday&filter=none" class="btn btn-warning">archives (some other articles)</Link>
+        <Link to="/archives?date=today&filter=hot"><button class="btn btn-danger">archives</button></Link>,
         <Link to="/settings/main" class="btn btn-success">settings</Link>
         <Link to="/settings/extra" class="btn btn-success">settings (extra)</Link>
         <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
